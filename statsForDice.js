8b3f4d8646827f3240924886d1bc4b7348d3b4f1
Marty Kane <marty@pixotech.com>
2016-07-14 13:31:23 -0500

    clean up the main script a little

--- statsForDice.js
+++ statsForDice.js
@@ -2,26 +2,45 @@
 var dice = require('./dice');
 
 var commandLineOptionDice = dice.parseDiceFromCommandLine(process.argv[2]);
+var commandLineOptionTarget = process.argv[3];
 
 var numberOfDice = commandLineOptionDice.number;
 var sizeOfDice = commandLineOptionDice.size;
 
 var rolls = dice.combineDice(numberOfDice, sizeOfDice);
 var stats = dice.getPercentageStatsFromTotals(dice.combineTotals(rolls));
+printStatsForAllRolls(stats);
+printTargetOdds(stats, commandLineOptionTarget);
 
-console.log('Stats for ' + numberOfDice + ' ' + sizeOfDice + '-sided dice:');
 
-for (roll in stats) {
-    var chance = stats[roll];
-    var line = padLeft(2)(roll)+': ' + padLeft(4)(chance)+'%';
-    console.log(line)
+function printStatsForAllRolls(stats) {
+    console.log('Stats for ' + numberOfDice + ' ' + sizeOfDice + '-sided dice:');
+
+    for (roll in stats) {
+        var chance = stats[roll];
+        var line = padLeft(2)(roll) + ': ' + padLeft(4)(chance) + '%';
+        console.log(line)
+    }
 }
 
-function padLeft(size){
-    return function(string) {
+function printTargetOdds(stats, targetArgument) {
+    console.log();
+    var target = Number.parseInt(targetArgument);
+
+    if ( target > 0 ) {
+        console.log('Odds of rolling higher than ' + target + ': ' + dice.sumPercentagesGreaterThanRoll(stats, target));
+    }
+    else {
+        target = -target;
+        console.log('Odds of rolling less than ' + target + ': ' + dice.sumPercentagesLessThanRoll(stats, target));
+    }
+}
+
+function padLeft(size) {
+    return function (string) {
         var padding = '';
         var missingLength = size - string.length + 1;
-        if ( missingLength > 0 ) {
+        if (missingLength > 0) {
             padding = new Array(missingLength).join(' ');
         }
         return padding + string;
